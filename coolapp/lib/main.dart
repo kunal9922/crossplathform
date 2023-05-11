3b373cb462ee5488f26d2fa2cc24cade4031c935
Kunal Soni <kunalsoni9200@gmail.com>
2023-05-11 20:23:45 +0530

    Working with stack widgets

--- coolapp/lib/main.dart
+++ coolapp/lib/main.dart
@@ -11,21 +11,20 @@ class MpApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.deepPurple,
-            title: const Text("Flutter Is Fun! "),
-          ),
-          body: const Column(
-            children: [
-              Icon(Icons.abc),
-              Icon(Icons.leaderboard),
-              Icon(Icons.place),
-              Row(children: [
-                Icon(Icons.add_circle),
-                Icon(Icons.air),
-              ],)
-          ],)
-          ),
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: const Text("Flutter Is Fun! "),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              color: Colors.indigo,
+              width: 100, height: 100,
+            ),
+            const Icon(Icons.verified),
+          ],
+        ),
+      ),
     );
   }
 }
